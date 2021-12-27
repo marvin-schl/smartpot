aac439b2f51f808c947fcf88178219db3fe9f46f
Marvin Schlageter <marvin.schlageter@haw-hamburg.de>
2021-12-27 16:53:29 +0100

    debugging adc channel2

--- SmartPot/adc.py
+++ SmartPot/adc.py
@@ -9,10 +9,12 @@ class MCP3426:
         self.slave_address = 0x68
         self.smbus = smbus.SMBus(1)
 
-    def _read_ch(self,conf_bits):
+
+    def read_ch1(self):
         """ read the signal-value of channel a from the MCP3426
             and return the voltage in volt """
-        self.smbus.write_byte(self.slave_address, conf_bits)
+        self.smbus.write(0x04)
+        self.smbus.write_byte(self.slave_address, 0x10)
         data = self.smbus.read_i2c_block_data(self.slave_address, 0x00, 2)
         # Convert the data to 12-bits
         raw_adc = (data[0] & 0x0F) * 256 + data[1]
@@ -20,8 +22,14 @@ class MCP3426:
             raw_adc -= 4095
         return raw_adc
 
-    def read_ch1(self):
-        return self._read_ch(0x10)
-
     def read_ch2(self):
-        return self._read_ch(0x30)
+        """ read the signal-value of channel a from the MCP3426
+            and return the voltage in volt """
+        self.smbus.write(0x04)
+        self.smbus.write_byte(self.slave_address, 0x30)
+        data = self.smbus.read_i2c_block_data(self.slave_address, 0x00, 2)
+        # Convert the data to 12-bits
+        raw_adc = (data[0] & 0x0F) * 256 + data[1]
+        if raw_adc > 2047:
+            raw_adc -= 4095
+        return raw_adc
