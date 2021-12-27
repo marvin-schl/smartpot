327aafd051a383a8e418a1d243269a83dddf61af
Marvin Schlageter <marvin.schlageter@haw-hamburg.de>
2021-12-27 16:07:08 +0100

    debugging adc channel2

--- SmartPot/adc.py
+++ SmartPot/adc.py
@@ -9,29 +9,19 @@ class MCP3426:
         self.slave_address = 0x68
         self.smbus = smbus.SMBus(1)
 
-    def _swap_bytes(self, word):
-        """ swap the two bytes of the given word
-            example: word = 0xABCD -> return 0xCDAB """
-        return ((word << 8) & 0xFF00) + (word >> 8)
-
-    def _two_complement(self, word):
-        """ convert the given word in two's complement to an integer with
-        sign """
-        if (word >= 0x8000):
-            return -((0xFFFF - word) + 1)
-        else:
-            return word
-
-    def _read_ch(self,channel):
+    def _read_ch(self,conf_bits):
         """ read the signal-value of channel a from the MCP3426
             and return the voltage in volt """
-        rdwo = self.smbus.read_word_data(self.slave_address, channel)
-        word = self._swap_bytes(rdwo)
-        voltage = self._two_complement(word)
-        return voltage / 1000
+        bus.write_byte(0x68, conf_bits)
+        data = data = self.smbus.read_i2c_block_data(0x68, 0x00, 2)
+        # Convert the data to 12-bits
+        raw_adc = (data[0] & 0x0F) * 256 + data[1]
+        if raw_adc > 2047:
+            raw_adc -= 4095
+        return raw_adc
 
     def read_ch1(self):
-        return self._read_ch(0x00)
+        return self._read_ch(0x10)
 
     def read_ch2(self):
-        return self._read_ch(0x20)
+        return self._read_ch(0x30)
